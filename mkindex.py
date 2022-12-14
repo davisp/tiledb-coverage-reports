79073fee8222559bea796e1ab78183f48f34076f
Paul J. Davis <paul.davis@tiledb.com>
2022-12-14 13:36:52 -0600

    Create an index

---
+++ mkindex.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python3
+
+"""
+Build index from directory listing
+
+mkindex.py </path/to/directory> [--header <header text>]
+
+From: https://stackoverflow.com/posts/39402604/revisions
+"""
+
+INDEX_TEMPLATE = r"""
+<html>
+<body>
+<h2>${header}</h2>
+<p>
+% for name in names:
+    <li><a href="${name}">${name}</a></li>
+% endfor
+</p>
+</body>
+</html>
+"""
+
+EXCLUDED = ['index.html']
+
+import os
+import argparse
+
+# May need to do "pip install mako"
+from mako.template import Template
+
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("directory")
+    parser.add_argument("--header")
+    args = parser.parse_args()
+    fnames = [fname for fname in sorted(os.listdir(args.directory))
+              if fname not in EXCLUDED]
+    header = (args.header if args.header else os.path.basename(args.directory))
+    print(Template(INDEX_TEMPLATE).render(names=fnames, header=header))
+
+
+if __name__ == '__main__':
+    main()
